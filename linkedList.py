a33742c6dd75597f7d7ccc138903f126a5a391ad
sknapper4 <knapperstephen@gmail.com>
2019-02-15 11:41:36 -0500

    first commit

---
+++ linkedList.py
@@ -0,0 +1,20 @@
+
+class LinkedList:
+
+    def __init__(self, node_data=None, previous_node=None, next_node=None):
+        self.node_data = node_data
+        self.previous_node = previous_node
+        self.next_node = next_node
+
+    def __str__(self):
+        return f'{self.node_data}'
+
+    def is_head(self):
+        if self.previous_node is None:
+            return True
+        return False
+
+    def is_tail(self):
+        if self.next_node is None:
+            return True
+        return False
